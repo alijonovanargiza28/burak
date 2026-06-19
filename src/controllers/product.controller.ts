738a353bb216e11dfa9721ed6a848cee6d661c97
Nargiza <alijonovanargiza38@gmail.com>
2026-06-19 11:01:20 +0500

    feat:modify products page besed on productd data

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -13,6 +13,7 @@ productController.getAllProducts = async(req:Request, res: Response)=>{
         console.log("getAllProducts")
         const data = await productService.getAllProduct();
         console.log("data",data)
+        
         res.render("products", {products:data})
     }catch(err){
         console.log("Error,getAllProducts", err);
