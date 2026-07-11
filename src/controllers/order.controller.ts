4890b6e19d9c4e9e9b04b2b518c92671a5769817
Nargiza <alijonovanargiza38@gmail.com>
2026-07-11 10:40:35 +0500

    feat:fix

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -36,7 +36,7 @@ orderController.getMyOrders = async (req: ExtendedRequest, res: Response) => {
    console.log(inquery)
    const result = await orderService.getMyOrders(req.member, inquery)
 
-    res.status(HttpCode.CREATED).json({});
+    res.status(HttpCode.CREATED).json({ result });
   } catch (err) {
     console.log("Error,createOrder", err);
     if (err instanceof Errors) res.status(err.code).json(err);
@@ -50,7 +50,7 @@ orderController.updateOrder = async (req: ExtendedRequest, res: Response) => {
     const input:OrderUpdateInput= req.body;
      
 
-     res.status(HttpCode.CREATED).json({});
+     res.status(HttpCode.CREATED).json({ result });
     }
     catch (err) {
     console.log("Error,createOrder", err);
